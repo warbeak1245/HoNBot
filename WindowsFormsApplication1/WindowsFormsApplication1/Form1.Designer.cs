a2bd04dc8d4832434ffe20aa214a76af8a0a25ed
Daniel Beckmann <beckmadj@rose-hulman.edu>
2011-04-14 20:26:06 -0400

    first commit, test cases not yet uploaded but we wrote them first :/

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.MainGameLoop = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(87, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Call MIAs";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 39);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Call REs";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(134, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Tower Dive Alert";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 93);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(162, 21);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Global Spell CD Alert";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(12, 120);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(119, 21);
+            this.checkBox5.TabIndex = 6;
+            this.checkBox5.Text = "Last Hit Assist";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(226, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 50);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(226, 51);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 48);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Forums";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // MainGameLoop
+            // 
+            this.MainGameLoop.Tick += new System.EventHandler(this.MainGameLoop_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 180);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "HoN  Bot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer MainGameLoop;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public bool callMissing = false;
+        public bool callReturn = false;
+        public bool callTowerDives = false;
+        public bool callGlobalSpells = false;
+        public bool lastHitAssist = false;
+
+        public bool isRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start()
+        {
+            isRunning = true;
+            //inject DLL, pass lastHitAssist in the constructor to the DLL
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        public void toggleMia()
+        {
+            callMissing = !callMissing;
+        }
+
+        public void toggleRe()
+        {
+            callReturn = !callReturn;
+        }
+
+        public void toggleTower()
+        {
+            callTowerDives = !callTowerDives;
+        }
+
+        public void toggleGlobal()
+        {
+            callGlobalSpells = !callGlobalSpells;
+        }
+
+        public void toggleAssist()
+        {
+            lastHitAssist = !lastHitAssist;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            toggleMia();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            toggleRe();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            toggleTower();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            toggleGlobal();
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            toggleAssist();
+        }
+
+        private void MainGameLoop_Tick(object sender, EventArgs e)
+        {
+            while (isRunning)
+            {
+                //logic to handle player information and throw events
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            end();
+        }
+
+        private void end()
+        {
+            isRunning = false;
+        }
+
+
+    }
+}
